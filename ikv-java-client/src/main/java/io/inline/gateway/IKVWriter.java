700086536abf7289e6246a00da9fe076b9fa17d1
Pushkar Gupta <pushkar.moi@gmail.com>
2023-12-04 22:53:18 -0800

    schema update events

--- ikv-java-client/src/main/java/io/inline/gateway/IKVWriter.java
+++ ikv-java-client/src/main/java/io/inline/gateway/IKVWriter.java
@@ -6,12 +6,12 @@ import com.inlineio.schemas.Streaming.*;
 import io.inline.gateway.streaming.KafkaProducerFactory;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.util.*;
 
 public class IKVWriter {
+    private static final FieldValue BROADCAST_KEY = FieldValue.newBuilder().build();  // empty no op key
+
     private final Producer<FieldValue, IKVDataEvent> _kafkaProducer;
 
     public IKVWriter() {
@@ -40,6 +40,7 @@ public class IKVWriter {
             Preconditions.checkNotNull(ExtractorUtils.extractPrimaryKeyAsString(context, fieldMap));
             FieldValue kafkaPartitioningKey = ExtractorUtils.extractPartitioningKeyValue(context, fieldMap);
 
+
             // TODO: filter out unknown fields by fetching schema
             IKVDocumentOnWire document = IKVDocumentOnWire.newBuilder().putAllDocument(fieldMap).build();
 
@@ -47,7 +48,6 @@ public class IKVWriter {
                     .setEventHeader(EventHeader
                             .newBuilder()
                             .build())
-                    .addAllFieldSchema(createFieldSchemaList(context, document))
                     .setUpsertDocumentFieldsEvent(
                             UpsertDocumentFieldsEvent
                                     .newBuilder()
@@ -76,8 +76,7 @@ public class IKVWriter {
      */
     public void publishDocumentDeletes(UserStoreContext context, Collection<Map<String, FieldValue>> documentIds) throws IllegalArgumentException, InterruptedException {
         Preconditions.checkNotNull(context);
-        Preconditions.checkNotNull(documentIds);
-        if (documentIds.isEmpty()) {
+        if (documentIds == null || documentIds.isEmpty()) {
             return;
         }
 
@@ -94,7 +93,6 @@ public class IKVWriter {
                     .setEventHeader(EventHeader
                             .newBuilder()
                             .build())
-                    .addAllFieldSchema(createFieldSchemaList(context, documentIdOnWire))
                     .setDeleteDocumentEvent(
                             DeleteDocumentEvent
                                     .newBuilder()
@@ -110,28 +108,35 @@ public class IKVWriter {
         }
     }
 
-    /**
-     * Construct field schema object based on the downstream event.
-     */
-    private static List<FieldSchema> createFieldSchemaList(UserStoreContext context, IKVDocumentOnWire document) {
-        Map<String, ?> documentMap = document.getDocumentMap();
-        List<FieldSchema> schema = new ArrayList<>(documentMap.size());
-
-        for (String name : documentMap.keySet()) {
-            Optional<FieldSchema> maybeSchema = context.fieldSchema(name);
-            maybeSchema.ifPresent(schema::add);
+    // Broadcast schema updates to all partitions for reader clients.
+    // Ok to fail partially and propagate error.
+    public void publishFieldSchemaUpdates(UserStoreContext context, Collection<FieldSchema> newFieldsToAdd) throws InterruptedException {
+        Preconditions.checkNotNull(context);
+        if (newFieldsToAdd == null || newFieldsToAdd.isEmpty()) {
+            return;
         }
 
-        return schema;
-    }
+        IKVDataEvent event = IKVDataEvent.newBuilder()
+                .setEventHeader(EventHeader
+                        .newBuilder()
+                        .build())
+                .setUpdateFieldSchemaEvent(UpdateFieldSchemaEvent.newBuilder()
+                        .addAllNewFieldsToAdd(newFieldsToAdd)
+                        .build())
+                .build();
 
+        // Broadcast!
+        int numPartitions = context.numKafkaPartitions();
+        for (int i = 0; i < numPartitions; i++) {
+            ProducerRecord<FieldValue, IKVDataEvent> producerRecord = new ProducerRecord<>(context.kafkaTopic(), 0, BROADCAST_KEY, event);
+            blockingPublishWithRetries(producerRecord, 3);  // can throw
+        }
+    }
 
     private void blockingPublishWithRetries(ProducerRecord<FieldValue, IKVDataEvent> record, int numRetries) throws InterruptedException {
         for (int i = 0; i < numRetries; i++) {
             try {
-                // TODO block on get() - can be problematic based on batching
-                _kafkaProducer.send(record).get();
-
+                _kafkaProducer.send(record);
                 return;
             } catch (Exception e) {
                 // TODO: add logging
