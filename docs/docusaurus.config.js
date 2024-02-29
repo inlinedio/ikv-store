e35507100bbabb2c23474b8ac036e8a8e5ef7b64
Pushkar Gupta <pushkar.moi@gmail.com>
2024-02-28 21:55:31 -0800

    built documentation

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,19 +8,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Docs',
+  title: 'IKV Documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://docs.inlined.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'inlined.io', // Usually your GitHub org/user name.
+  projectName: 'ikv-store', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/inlinedio/ikv-store/tree/main/docs/',
+            'https://github.com/inlinedio/ikv-store/tree/master/docs/',
         },
         blog: false,
         theme: {
