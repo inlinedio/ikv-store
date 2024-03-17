f24c4a3a51b708ae2820a385d3d332ebc8c79998
Pushkar Gupta <pushkar.moi@gmail.com>
2024-03-16 17:37:44 -0700

    remove duplicate proto defs in ikv rust

--- ikv/build.rs
+++ ikv/build.rs
@@ -8,17 +8,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Use `protoc-bin-vendored` bundled protoc command, optional.
         .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
         // All inputs and imports from the inputs must reside in `includes` directories.
-        .includes(&["src/proto", "src/proto/internal"])
+        .includes(&["../ikv-cloud/src/main/proto"])
         .inputs(&[
-            "src/proto/common.proto",
-            "src/proto/streaming.proto",
-            "src/proto/internal/index.proto",
+            "../ikv-cloud/src/main/proto/common.proto",
+            "../ikv-cloud/src/main/proto/streaming.proto",
+            "../ikv-cloud/src/main/proto/index.proto",
         ])
         // Specify output directory relative to Cargo output directory.
         .cargo_out_dir("protos")
         .run_from_script();
 
-    tonic_build::compile_protos("src/proto/common.proto")?;
-    tonic_build::compile_protos("src/proto/services.proto")?;
+    //tonic_build::compile_protos("src/proto/common.proto")?;
+    //tonic_build::compile_protos("src/proto/services.proto")?;
     Ok(())
 }
