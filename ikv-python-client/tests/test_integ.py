eeeb473ddce9c5fdb48c7289148eabdd1f79182f
Pushkar Gupta <pushkar.moi@gmail.com>
2024-03-23 17:31:05 -0700

    retry policy syntax

--- ikv-python-client/tests/test_integ.py
+++ ikv-python-client/tests/test_integ.py
@@ -40,41 +40,41 @@ class TestInteg(unittest.TestCase):
         self.assertEqual("Alice", self.reader.get_string_value("0", "firstname"))
 
         # read valid fields for invalid document
-        self.assertIsNone(self.reader.get_string_value("1", "userid"))
+        self.assertIsNone(self.reader.get_string_value("1000", "userid"))
 
         # read invalid fields for valid document
         self.assertIsNone(self.reader.get_string_value("0", "foo_field"))
         
 
     def test_upsert_delete_doc(self):
-        # upsert {"userid": "0", "firstname": "Alice"}
-        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "0")\
+        # upsert {"userid": "1", "firstname": "Alice"}
+        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "1")\
             .put_string_field("firstname", "Alice").build()
         self.writer.upsert_fields(document)
        
-        # delete {"userid": "0"}
-        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "0").build()
+        # delete {"userid": "1"}
+        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "1").build()
         self.writer.delete_document(document)
 
         time.sleep(5)
 
         # read assertions
-        self.assertIsNone(self.reader.get_string_value("0", "userid"))
-        self.assertIsNone(self.reader.get_string_value("0", "firstname"))
+        self.assertIsNone(self.reader.get_string_value("1", "userid"))
+        self.assertIsNone(self.reader.get_string_value("1", "firstname"))
 
     def test_upsert_drop_fields(self):
-        # upsert {"userid": "0", "firstname": "Alice"}
-        # upsert {"userid": "1", "firstname": "Bob", "city": "NYC"}
-        # upsert {"userid": "2", "firstname": "Allison", "city": "NYC"}
-        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "0")\
+        # upsert {"userid": "5", "firstname": "Alice"}
+        # upsert {"userid": "6", "firstname": "Bob", "city": "NYC"}
+        # upsert {"userid": "7", "firstname": "Allison", "city": "NYC"}
+        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "5")\
             .put_string_field("firstname", "Alice").build()
         self.writer.upsert_fields(document)
         
-        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "1")\
+        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "6")\
             .put_string_field("firstname", "Bob").put_string_field("city", "NYC").build()
         self.writer.upsert_fields(document)
         
-        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "2")\
+        document = ikv_py.document.IKVDocumentBuilder().put_string_field("userid", "7")\
             .put_string_field("firstname", "Allison").put_string_field("city", "NYC").build()
         self.writer.upsert_fields(document)
 
@@ -85,17 +85,17 @@ class TestInteg(unittest.TestCase):
         time.sleep(5)
 
         # read assertions
-        self.assertEqual("0", self.reader.get_string_value("0", "userid"))
-        self.assertEqual("1", self.reader.get_string_value("1", "userid"))
-        self.assertEqual("2", self.reader.get_string_value("2", "userid"))
+        self.assertEqual("5", self.reader.get_string_value("5", "userid"))
+        self.assertEqual("6", self.reader.get_string_value("6", "userid"))
+        self.assertEqual("7", self.reader.get_string_value("7", "userid"))
         
-        self.assertIsNone(self.reader.get_string_value("0", "firstname"))
-        self.assertIsNone(self.reader.get_string_value("1", "firstname"))
-        self.assertIsNone(self.reader.get_string_value("2", "firstname"))
+        self.assertIsNone(self.reader.get_string_value("5", "firstname"))
+        self.assertIsNone(self.reader.get_string_value("6", "firstname"))
+        self.assertIsNone(self.reader.get_string_value("7", "firstname"))
         
-        self.assertIsNone(self.reader.get_string_value("0", "city"))
-        self.assertIsNone(self.reader.get_string_value("1", "city"))
-        self.assertIsNone(self.reader.get_string_value("2", "city"))
+        self.assertIsNone(self.reader.get_string_value("5", "city"))
+        self.assertIsNone(self.reader.get_string_value("6", "city"))
+        self.assertIsNone(self.reader.get_string_value("7", "city"))
 
 # run: $> python3 tests/test_native_reader.py
 #      $> [ikv-python-client/tests]$ python3 -m unittest
